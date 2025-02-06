dc11e779075806e331ece233b3f517f87b1cfd99
Kamran281 <kamranmuhammad14660@gmail.com>
2025-02-06 18:52:32 +0500

    bug fix

--- backend/app.js
+++ backend/app.js
@@ -3,43 +3,55 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
+import cloudinary from "cloudinary";
+
 import { dbConnection } from "./database/dbConnection.js";
-import messageRouter from "./router/messageRouter.js";
 import { errorMiddleware } from "./middlewares/error.js";
+import messageRouter from "./router/messageRouter.js";
 import userRouter from "./router/userRouter.js";
 import appointmentRouter from "./router/appointmentRouter.js";
 
-const app = express();
+// Load environment variables
 dotenv.config();
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+// Initialize Express app
+const app = express();
+
+// Configure Cloudinary
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Connect to database
+dbConnection();
+
+// Middleware
+app.use(cors({
+  origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: "/tmp/", // Ensure this directory exists or use a relative path like './temp/'
+  createParentPath: true, // Ensures the directory is created if it doesn't exist
+}));
 
-app.get("/", (req, res) => {
-  res.send("Hello World...!");
-});
-
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: "/tmp/", // Ensure this directory exists or use a relative path like './temp/'
-    createParentPath: true, // Ensures the directory is created if it doesn't exist
-  })
-);
-
+// Routes
+app.get("/", (req, res) => res.send("Hello World...!"));
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/appointment", appointmentRouter);
 
-dbConnection();
-
+// Error handling middleware
 app.use(errorMiddleware);
-export default app;
+
+// Start server
+const PORT = process.env.PORT || 4000;
+console.log(PORT)
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
